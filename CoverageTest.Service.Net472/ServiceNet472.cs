d51a6702f6933c6bab33721dcb650f29247f8e26
Alessandro <alessandroste@users.noreply.github.com>
2021-05-21 17:15:25 +0200

    initial commit

---
+++ CoverageTest.Service.Net472/ServiceNet472.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoverageTest.Service
+{
+    public class ServiceNet472
+    {
+        private readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        public async Task<string> WorkAsync()
+        {
+            await this.semaphore.WaitAsync();
+            return "done";
+        }
+
+        public async Task<string> WorkAsync2()
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(1));
+            return "done";
+        }
+    }
+}
---
+++ CoverageTest.Tests/MyServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using CoverageTest.Service;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoverageTest
+{
+    public class MyServiceTests
+    {
+        [Fact]
+        public async Task TestAsyncStd()
+        {
+            var service472 = new ServiceNet472();
+            await service472.WorkAsync();
+            await service472.WorkAsync2();
+            var serviceStd = new ServiceNetStd();
+            await serviceStd.WorkAsync();
+            await serviceStd.WorkAsync2();
+        }
+    }
+}
